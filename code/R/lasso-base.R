ce7661d6c50eca59037714e639755cd69179bcd0
lmudl <dario.lepke@gmail.com>
2021-02-27 19:26:24 +0100

    lets go

--- code/R/lasso-base.R
+++ code/R/lasso-base.R
@@ -10,42 +10,45 @@
 # y = t + point + seasonal + point::seasonal 
 setwd("MA-climate")
 library(dplyr)
+library(MASS)
+library(glmnet)
+
+# load both data sets
 load("data/processed/hadcrut-processed.Rda")
 load("data/processed/cru-processed.Rda")
+
+# for drought col is named wrongly
 drought %>% rename(drought = temp) -> drought
-any(is.na(drought$drought))
-t1 <- head(drought)
-drought %>% group_by(time) %>% dplyr::summarise(drought_mean = mean(drought, na.rm = TRUE))
-t2 <- head(temp)
+any(is.na(drought$drought)) # FALSE
 drought %>% group_by(time) %>% 
   dplyr::summarise(drought_mean = mean(drought, na.rm = TRUE)) -> drought
 
-base::merge(drought, temp, by = "time") -> data
-library(MASS)
-library(glmnet)
-data %>% mutate(coord = str(coord)) 
-head(data)
-X <- data %>% dplyr::select(time, temp, coord)
-Y <- data %>% dplyr::select(drought_mean)
+t_drought <- drought %>% filter(time > 1300)
+t_temp <- temp %>% filter(time > 1300)
+t_temp <- na.omit(t_temp)
+base::merge(t_drought, t_temp, by = "time") -> data
+any(is.na(data)) #FALSE
+
+data$coord <- factor(data$coord)
+X <- model.matrix(~., dplyr::select(data, -drought_mean))
+Y <- data %>% dplyr::select(drought_mean) %>% as.matrix()
 fit_lasso <- glmnet(X, Y, family = "gaussian", alpha = 1)
+print(fit_lasso)
+plot(fit_lasso)
+head(fit_lasso)
+length(coef(fit_lasso))
+dim(X)
+
+nz <- coef(fit_lasso)[,1] != 0
+rownames(coef(fit_lasso))[nz]
 
 # do one regression model for each coordinate?
 etst <- model.matrix(~ time + temp + as.factor(coord), temp)
 head(temp)
 
-#mtcars example
-data("mtcars")
-#add cat toy variable
-mtcars$cat <- factor(rep(letters[1:8], 4))
-# factor(head(temp$coord))
-X <- model.matrix(~. , dplyr::select(mtcars, -mpg))
-fit <- glmnet(X, y = mtcars$mpg)
-print(fit)
-plot(fit)
-coef(fit)
-cvfit <- cv.glmnet(X, mtcars$mpg)
-plot(cvfit)
-cvfit$lambda.min
 
 temp$coord <- factor(temp$coord)
 X <- model.matrix(~., dplyr::select(temp, -temp))
+head(t_drought)
+head(t_temp)
+head(data)
